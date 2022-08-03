10f2a98284e7981a4e327d037c4a820cb0821508
m.papucci@studenti.unipi.it <m.papucci@studenti.unipi.it>
2022-08-03 18:53:06 +0200

    fixed bug in test file report

--- src/libraries/experiment.py
+++ src/libraries/experiment.py
@@ -69,7 +69,7 @@ class NLMExperiment:
         if path_to_file:
             with open(path_to_file, 'w') as output:
                 output.write("y_true\ty_proba\ty_pred\n")
-                predictions = self.test_results.predictions
+                predictions = np.max(self.test_results.predictions)
                 predicted_classes = np.argmax(predictions, axis=-1)
                 for y_true, y_proba, y_pred in zip(self.test_results.label_ids, predictions, predicted_classes):
                     output.write(str(y_true) + '\t' + str(y_proba) + str(y_pred) + '\n')
